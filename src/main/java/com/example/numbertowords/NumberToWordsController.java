eeb4250b345b2eb8dedcc13b1abc8608e810219a
gabrielbarneaud <56385781+gabrielbarneaud@users.noreply.github.com>
2025-05-04 18:22:06 +0200

    Update NumberToWordsController.java
    Adding Error 400 for too large values (one billion and more)

--- src/main/java/com/example/numbertowords/NumberToWordsController.java
+++ src/main/java/com/example/numbertowords/NumberToWordsController.java
@@ -2,6 +2,7 @@ package com.example.numbertowords;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/convert")
@@ -9,6 +10,12 @@ public class NumberToWordsController {
 
     @GetMapping
     public ResponseEntity<String> convert(@RequestParam int number) {
+        if (Math.abs(number) > 999_999_999) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Number too large. Please enter a number between -999999999 and 999999999.");
+        }
+
         String result = NumberToWordsConverter.convert(number);
         return ResponseEntity.ok(result);
     }
